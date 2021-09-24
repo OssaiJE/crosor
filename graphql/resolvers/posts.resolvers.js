9c3edd208df948327af4ac0d539fbd4b990a1f94
Julius Ossai <ossaije1@gmail.com>
2021-09-24 19:37:58 +0100

    like and unlike post now works

--- graphql/resolvers/posts.resolvers.js
+++ graphql/resolvers/posts.resolvers.js
@@ -109,5 +109,30 @@ module.exports = {
         throw new UserInputError("Post not found");
       }
     },
+
+    //  Like and unlike post mutation
+    likePost: async (_, { postId }, context) => {
+      //  the below username was destructured from user
+      const { username } = authCheck(context);
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //  Post already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          //  Post not liked, like post
+          post.likes.push({
+            username,
+            createdat: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found.");
+      }
+    },
   },
 };
