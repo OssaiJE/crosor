646fe9bfaf83910327c3d93a25d9e41de41d8677
Julius Ossai <ossaije1@gmail.com>
2021-09-28 02:08:44 +0100

    started working on the react client

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,24 @@
+import React from "react";
+import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+// export default (
+//   <ApolloProvider client={client}>
+//     <App/>
+//   </ApolloProvider>
+// );
+
+const ApolloP = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+export default ApolloP;
---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloP from "./ApolloProvider";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloP />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
