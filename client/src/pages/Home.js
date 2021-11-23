770212c94f546408e217a67d046c3d0a056412cd
OssaiJE <ossaije1@gmail.com>
2021-11-23 23:59:29 +0100

    updated the client

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
-// import GuestHeader from '../components/GuestHeader'
 // import RegLogin from '../components/RegLogin'
 
 const Home = () => {
-    return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    );
-}
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
