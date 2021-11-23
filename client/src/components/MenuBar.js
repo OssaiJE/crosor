b99c278255ff20cbceb8d90bbe4631d18fbb65cb
OssaiJE <ossaije1@gmail.com>
2021-11-24 00:12:23 +0100

    updated the navbar

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
 const MenuBar = () => {
-  const [activeItem, setActiveItem] = useState("");
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="large" color="violet">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
